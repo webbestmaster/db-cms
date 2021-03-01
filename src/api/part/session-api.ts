871fdd2a5c952f4a15f56662d4b25e9a09bfcd7c
dim <web.best.master@gmail.com>
2021-03-01 21:46:16 +0300

    WIP

--- src/api/part/session-api.ts
+++ src/api/part/session-api.ts
@@ -15,6 +15,29 @@ export type UserSessionType = {
     destroy: (callBack?: unknown) => unknown;
 };
 
+/*
+
+import crypto from 'crypto';
+
+export type UserLoginPasswordType = {login: string, password: string};
+
+export async function getUserByLogin(login: string): Promise<MongoUserType | null> {
+    const collection = await getCollection<MongoUserType>(dataBaseConst.name, dataBaseConst.collection.user);
+
+    if (isError(collection)) {
+        throw new Error(`Can not get collection: ${dataBaseConst.collection.user}`);
+    }
+
+    return collection.findOne({login});
+}
+
+export function getPasswordSha256(text: string): string {
+    const sha256PasswordHmac = crypto.createHmac('sha256', passwordKey);
+
+    return sha256PasswordHmac.update(text).digest('hex');
+}
+*/
+
 export function addSessionApi(app: Application): void {
     app.get(apiRouteMap.auth.login, async (request: Request, response: Response) => {
         const {login, password} = request.body;
