36d4f0a11759c22eafd274d823f2979d325a356b
dim <web.best.master@gmail.com>
2021-03-14 20:20:02 +0300

    WIP

--- mongo-start.sh
+++ mongo-start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+mkdir ./db/db-1
+mkdir ./db/db-2
+mkdir ./db/db-3
+mkdir ./db/db-4
+
 mongod --config ./mongodb-1.config
 mongod --config ./mongodb-2.config
 mongod --config ./mongodb-3.config
